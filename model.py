47aa9ebaa723823f5dd694bca75439766ede54a9
Naraymond <naraymond@expedia.com>
2021-01-07 21:16:11 +0000

    Add prototype notebook

--- model.py
+++ model.py
@@ -3,3 +3,9 @@ import numpy as np
 import pandas as pd
 from itertools import combinations_with_replacement
 from collections import OrderedDict
+
+
+n_goals = [0, 1, 2, 3, 4, 5]
+
+def poisson_pmf(k, lam):
+    return (lam**k*np.exp(-lam))/np.math.factorial(k)

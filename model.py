9cdeb8972ec31a201179e2de3570d50c43eb4d19
Naraymond <naraymond@expedia.com>
2021-01-07 21:48:58 +0000

    numpy troubleshooting

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
-from scipy.stats import poisson
 import numpy as np
 import pandas as pd
 from itertools import combinations_with_replacement
 from collections import OrderedDict
 
+np.set_printoptions(suppress=True)
 
 n_goals = [0, 1, 2, 3, 4, 5]
 
 def poisson_pmf(k, lam):
     return (lam**k*np.exp(-lam))/np.math.factorial(k)
+
+print(poisson_pmf(0,1.23))

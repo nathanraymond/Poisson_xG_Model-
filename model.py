6933c89222670a2741352fd21e6d8238174000dd
Naraymond <naraymond@expedia.com>
2021-01-09 17:02:55 +0000

    Create PoissonModel class object

--- model.py
+++ model.py
@@ -7,7 +7,43 @@ np.set_printoptions(suppress=True)
 
 n_goals = [0, 1, 2, 3, 4, 5]
 
-def poisson_pmf(k, lam):
-    return (lam**k*np.exp(-lam))/np.math.factorial(k)
+np.set_printoptions(suppress=True)
 
-print(poisson_pmf(0,1.23))
+
+class PoissonModel():
+	def __init__(self, lam_home, lam_away, probability):
+		self.lam_home = lam_home
+		self.lam_away = lam_away
+		self.probability = probability
+		self.n_goals = [0, 1, 2, 3, 4, 5]
+
+	def poisson_pmf_goals_home(self):
+	    probs = []
+	    for k in self.n_goals:
+	        probs.append((self.lam_home**k*np.exp(-self.lam_home))/np.math.factorial(k))
+	    return (np.array([probs,]*6)).T
+
+	def poisson_pmf_goals_away(self):
+	    probs = []
+	    for k in self.n_goals:
+	        probs.append((self.lam_away**k*np.exp(-self.lam_away))/np.math.factorial(k))
+	    return np.array([probs,]*6)
+
+	def get_probabilty_df(self):
+		prob = self.poisson_pmf_goals_home()*self.poisson_pmf_goals_away()
+		return pd.DataFrame(prob, columns = ['0','1','2','3','4','5'], index = ['0','1','2','3','4','5'])
+
+	def get_match_probability(self):
+		combs = list(OrderedDict.fromkeys(list(combinations_with_replacement(['0','1','2','3','4','5','4','3','2','1','0'], 2))))
+		if (self.probability).lower() == 'home':
+			return sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] > combs[i][1])
+		elif (self.probability).lower() == 'away':
+			return sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] < combs[i][1])
+		elif (self.probability).lower() == 'draw':
+			return sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] == combs[i][1])
+		elif (self.probability).lower() == 'all':
+			return sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] == combs[i][1]), sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] >= combs[i][1]), sum((self.get_probabilty_df()).loc[combs[i]] for i in range (0, len(combs)) if combs[i][0] <= combs[i][1])
+		
+
+
+print(PoissonModel(1.23, 0.997, 'all').get_match_probability())
